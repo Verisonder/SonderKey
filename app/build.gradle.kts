2ef464c3e6498b352b71bd794252be67e9416430
Verisonder <verisonder@users.noreply.github.com>
2026-08-10 16:17:33 +0000

    Prepare 4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.verisonder.sonderkey"
         minSdk = 21
         targetSdk = 35
-        versionCode = 143
-        versionName = "4.3"
+        versionCode = 144
+        versionName = "4.4"
 
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         
